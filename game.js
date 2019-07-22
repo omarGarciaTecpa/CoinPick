85ce2be16744d44fe6f1ce2cdd5a04e91c69bf8e
omarGarciaTecpa <omartecpa@gmail.com>
2019-07-21 22:06:50 -0500

    Background, Platforms and Falling objects

---
+++ game.js
@@ -0,0 +1,132 @@
+const game = new Phaser.Game(1080, 608, Phaser.AUTO, '', {
+    preload: preload,
+    create: create,
+    update: update
+});
+
+const tileOffset = 64;
+
+function preload() {
+    //Preload asset images 
+    game.load.image('background', 'assets/Background.png');
+    game.load.image('grass-mid', 'assets/Ground/GrassMid.png');
+    game.load.image('mace', 'assets/env/mace.png');
+    //game.load.spritesheet('woof', 'assets/woof.png', 32, 32);
+}
+
+let platforms;
+let damageItems;
+//let player;
+//let diamonds;
+//let scoreText;
+//let score = 0;
+//let cursors;
+
+
+function create() {
+    //start physics system
+    game.physics.startSystem(Phaser.Physics.Arcade);
+
+    //add the sprite for the sky background
+    game.add.sprite(0, 0, 'background');
+
+    //add a group for platforms
+    platforms = game.add.group();
+    platforms.enableBody = true;
+
+    createPlatform(platforms, 2 ,15, 2);
+    createPlatform(platforms, 3 , 5 , 4 );
+    createPlatform(platforms, 12, 14 , 5 );
+    createPlatform(platforms, 6, 10 , 6 );
+
+    damageItems = game.add.group();
+    damageItems.enableBody = true
+    createFallingItem(damageItems, 2, 10, 10);
+    
+
+    //set player
+    //player = game.add.sprite(32, game.world.height - 150, 'woof');
+    //game.physics.arcade.enable(player);
+    //player.body.bounce.y = 0.2;
+    //player.body.gravity.y = 800;
+    //player.body.collideWorldBounds = true;
+
+    //player.animations.add('left', [0, 1], 10, true);
+    //player.animations.add('right', [2, 3], 10, true);
+
+
+    //set diamonds
+    //diamonds = game.add.group();
+    //diamonds.enableBody = true;
+
+    /*
+    for (var i = 0; i < 12; i++) {
+        let diamond = diamonds.create(i * 70, 0, 'diamond');
+        diamond.body.gravity.y = 1000;
+        diamond.body.bounce.y = 0.3 + Math.random() * 0.2;
+        diamond.body.collideWorldBounds = true;
+    }*/
+
+    //scoreText = game.add.text(16, 16, '', { fontSize: '32px', fill: '#000' });
+    //cursors = game.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    game.physics.arcade.collide(damageItems, platforms);
+    //COllisions between player and platforms
+    /*game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(diamonds, platforms);
+    game.physics.arcade.overlap(player, diamonds, collectDiamond, null, this);
+
+    player.body.velocity.x = 0;*/
+
+    /*
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    }else{
+        player.animations.stop();
+    }*/
+
+    /*
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -400;
+    }*/
+
+
+}
+
+/*
+function collectDiamond(player, diamond) {
+    diamond.kill();
+    score += 10;
+    scoreText.text = 'SCORE: ' + score;
+}*/
+
+/**
+ * Creates a platform and adds it to a group.
+ * 
+ * Horizontally, at 0 would be the tile most to the left and at 17 the tile most to the right.
+ * Vertically, at 0 would be the tile on the bottom, and at 10 the tile on the top.
+ * @param {game group} group group where the platform is added
+ * @param {number} horizontalStart horizontal startPoint relative to the tile array. 
+ * @param {number} horizontalEnd horizontal endPoint relative to the tile array
+ * @param {number} verticalStart vertical startpoint of the platform
+ */
+function createPlatform(group, horizontalStart , horizontalEnd,  verticalStart ){
+    let ground;
+    for(i= horizontalStart; i < horizontalEnd; i++){
+        ground = group.create(i * tileOffset, game.world.height - (tileOffset * verticalStart), 'grass-mid');
+        ground.body.immovable = true;
+    }    
+}
+
+function createFallingItem(group, horizontalStart , verticalStart, damage){
+    let tempItem = group.create(horizontalStart * tileOffset , game.world.height - (verticalStart * tileOffset) , 'mace'  );
+    tempItem.body.gravity.y = 1000;
+    tempItem.body.bounce.y = 0.3;
+}
+
