79ecd550d3f35e32e3034a233d0e046dcca233fc
Omar Tecpa <omartecpa@gmail.com>
2019-07-22 12:48:52 -0500

    Added Battle timer

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const game = new Phaser.Game(1080, 608, Phaser.CANVAS, '', {
     preload: preload,
     create: create,
-    update: update
+    update: update,
+    render: render
 });
 
 const tileOffset = 64;
@@ -21,10 +22,12 @@ let player1Damage = 0;
 let player2Damage = 0;
 let player1DamageText = 0;
 let player2DamageText = 0;
+let timerText;
+let cursors;
 //let diamonds;
 //let scoreText;
 //let score = 0;
-//let cursors;
+
 
 
 function create() {
@@ -38,10 +41,10 @@ function create() {
     platforms = game.add.group();
     platforms.enableBody = true;
 
-    createPlatform(platforms, 2 ,15, 2);
-    createPlatform(platforms, 3 , 5 , 4 );
-    createPlatform(platforms, 12, 14 , 5 );
-    createPlatform(platforms, 6, 10 , 6 );
+    createPlatform(platforms, 2, 15, 2);
+    createPlatform(platforms, 3, 5, 4);
+    createPlatform(platforms, 12, 14, 5);
+    createPlatform(platforms, 6, 10, 6);
 
     damageItems = game.add.group();
     damageItems.enableBody = true;
@@ -70,9 +73,10 @@ function create() {
     }*/
 
 
-    //cursors = game.input.keyboard.createCursorKeys();
-    
+    cursors = game.input.keyboard.createCursorKeys();
+
     setPlayerText();
+    startTimer();
     scaleWindow();
 }
 
@@ -104,6 +108,14 @@ function update() {
 
 }
 
+
+function render() {
+    // If our timer is running, show the time in a nicely formatted way, else show 'Done!'
+    if (timer.running) {
+        timerText.text = formatTime(timerEvent.delay - timer.ms);
+    }
+}
+
 /*
 function collectDiamond(player, diamond) {
     diamond.kill();
@@ -121,41 +133,71 @@ function collectDiamond(player, diamond) {
  * @param {number} horizontalEnd horizontal endPoint relative to the tile array
  * @param {number} verticalStart vertical startpoint of the platform
  */
-function createPlatform(group, horizontalStart , horizontalEnd,  verticalStart ){
+function createPlatform(group, horizontalStart, horizontalEnd, verticalStart) {
     let ground;
-    for(i= horizontalStart; i < horizontalEnd; i++){
+    for (i = horizontalStart; i < horizontalEnd; i++) {
         ground = group.create(i * tileOffset, game.world.height - (tileOffset * verticalStart), 'grass-mid');
         ground.body.immovable = true;
-    }    
+    }
 }
 
 /**
- * 
+ * Creates a falling item. 
  * @param {game group} group 
  * @param {number} horizontalStart 
  * @param {number} verticalStart 
  * @param {number} damage 
  */
-function createFallingItem(group, horizontalStart , verticalStart, gravity){
+function createFallingItem(group, horizontalStart, verticalStart, gravity) {
     let tempItem = group.create(
-        horizontalStart * tileOffset , 
-        game.world.height - (verticalStart * tileOffset) , 
-        'mace'  
-        );
+        horizontalStart * tileOffset,
+        game.world.height - (verticalStart * tileOffset),
+        'mace'
+    );
     tempItem.body.gravity.y = gravity;
     tempItem.body.bounce.y = 0.2;
 }
 
-function setPlayerText(){
-    player1DamageText = game.add.text( 2* tileOffset, 16, '0', { fontSize: '64px', fill: '#000' });
-    player2DamageText = game.add.text(game.world.width -  2 * tileOffset , 16, '0', { fontSize: '64px', fill: '#000' });
+
+/** Setup Player related text*/
+function setPlayerText() {
+    player1DamageText = game.add.text(2 * tileOffset, 16, '0', { fontSize: '64px', fill: '#000' });
+    player2DamageText = game.add.text(game.world.width - 2 * tileOffset, 16, '0', { fontSize: '64px', fill: '#000' });
 }
 
-
-function scaleWindow(){
+/** Scale game window to window size*/
+function scaleWindow() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.scale.setScreenSize(true);
+}
+
+/**Set timer */
+function startTimer() {
+    timerText = game.add.text(8 * tileOffset, 16, '0', { fontSize: '64px', fill: '#000' });
+    timer = game.time.create();
+    // Create a delayed event 1m and 30s from now
+    timerEvent = timer.add(Phaser.Timer.MINUTE * 2 + Phaser.Timer.SECOND * 00, endBattle, game);
+    // Start the timer
+    timer.start();
+}
+
+function endBattle() {
+    // Stop the timer when the delayed event triggers
+    timer.stop();
+    //End battle events
+}
+
+/**
+ * Formats the time nively
+ * @param {*} miliseconds milliseconds from event
+ */
+function formatTime(miliseconds) {
+    let toSeconds = Math.round(miliseconds/ 1000);
+    // Convert seconds (s) to a nicely formatted and padded time string
+    var minutes = "0" + Math.floor(toSeconds / 60);
+    var seconds = "0" + (toSeconds - minutes * 60);
+    return minutes.substr(-2) + ":" + seconds.substr(-2);   
 }
 
